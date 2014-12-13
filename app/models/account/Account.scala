2a6663530dfda7cfba404feb7d661c37a522f301
gakuzzzz <gaku_github@t2v.jp>
2014-12-14 00:35:55 +0900

    Fixed OpenId Account aggregation

--- app/models/account/Account.scala
+++ app/models/account/Account.scala
@@ -2,15 +2,15 @@ package models.account
 
 import scalikejdbc._
 
-case class Account(id: AccountId, name: String, twitter: Option[TwitterAccount] = None, yahoo: Option[OpenIdAccount] = None)
+case class Account(id: AccountId, name: String, twitter: Option[TwitterAccount] = None, openIds: Seq[OpenIdAccount] = Vector())
 
 object Account extends SQLSyntaxSupport[Account] {
 
-  def apply(s: SyntaxProvider[Account])(rs: WrappedResultSet): Account = autoConstruct(rs, s, "twitter", "yahoo")
+  def apply(s: SyntaxProvider[Account])(rs: WrappedResultSet): Account = autoConstruct(rs, s, "twitter", "openIds")
 
   private val a = Account.syntax("a")
   private val t = TwitterAccount.syntax("t")
-  private val y = OpenIdAccount.syntax("y")
+  private val o = OpenIdAccount.syntax("o")
   private val ac = Account.column
 
   def findById(id: AccountId)(implicit session: DBSession): Option[Account] = {
@@ -18,14 +18,12 @@ object Account extends SQLSyntaxSupport[Account] {
       select
         .from(Account as a)
         .leftJoin(TwitterAccount as t).on(a.id, t.accountId)
-        .leftJoin(OpenIdAccount as y).on(a.id, y.accountId)
+        .leftJoin(OpenIdAccount as o).on(a.id, o.accountId)
       .where.eq(a.id, id)
-    }.map { rs =>
-      Account(a)(rs).copy(
-        twitter = rs.longOpt(t.resultName.id).map(_ => TwitterAccount(t)(rs)),
-        yahoo   = rs.stringOpt(y.resultName.id).map(_ => OpenIdAccount(y)(rs))
-      )
-    }.first().apply()
+    }.one(rs => Account(a)(rs).copy(twitter = rs.longOpt(t.resultName.id).map(_ => TwitterAccount(t)(rs))))
+      .toMany(rs => rs.stringOpt(o.resultName.id).map(_ => OpenIdAccount(o)(rs)))
+      .map((a, os) => a.copy(openIds = os))
+      .first().apply()
   }
 
   def insert(name: String)(implicit session: DBSession): Account = {
